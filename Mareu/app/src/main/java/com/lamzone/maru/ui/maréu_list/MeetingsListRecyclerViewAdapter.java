ed00781a96423c0569bc504f743d51b0ea5166a5
Daiqua <yoann.c.maillot@gmail.com>
2021-12-14 22:48:21 +0100

    added colored circle to indicate the room in MaReuActivity

--- Mareu/app/src/main/java/com/lamzone/maru/ui/maréu_list/MeetingsListRecyclerViewAdapter.java
+++ Mareu/app/src/main/java/com/lamzone/maru/ui/maréu_list/MeetingsListRecyclerViewAdapter.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.lamzone.maru.R;
 import com.lamzone.maru.di.DI;
 import com.lamzone.maru.model.Meeting;
@@ -32,6 +34,7 @@ public class MeetingsListRecyclerViewAdapter extends RecyclerView.Adapter<Meetin
         private final TextView meetingStartingHour;
         private final TextView meetingDuration;
         private final ImageButton deleteMeetingButton;
+        private final ImageView roomColor;
 
         public MyViewHolder(View view) {
             super(view);
@@ -41,6 +44,7 @@ public class MeetingsListRecyclerViewAdapter extends RecyclerView.Adapter<Meetin
             meetingStartingHour = view.findViewById(R.id.activity_meetings_item_meeting_starting_hour);
             meetingDuration = view.findViewById(R.id.activity_meetings_item_meeting_duration);
             deleteMeetingButton = view.findViewById(R.id.activity_meetings_item_delete_button);
+            roomColor = view.findViewById(R.id.activity_meeting_item_color);
         }
     }
 
@@ -63,17 +67,14 @@ public class MeetingsListRecyclerViewAdapter extends RecyclerView.Adapter<Meetin
         String attendeesEmailAddressesCommaSeparated = mApiService
                 .getAttendeesListEmailAddresses(meeting.getMeetingAttendeesList());
 
-        //set texts
+        //set content
         holder.meetingName.setText(meeting.getStrMeetingName());
         holder.meetingAttendeesList.setText(attendeesEmailAddressesCommaSeparated);
         holder.meetingStartingDate.setText("le "
                 + DateConvertor.convert_yyyy_MM_dd_to_dd_MMMM(meeting.getStrMeetingStartDate()));
         holder.meetingStartingHour.setText("à " + meeting.getStrMeetingStartHour());
         holder.meetingDuration.setText("Durée: " + meeting.getMeetingDuration() + " min");
-
-        //set colors
-        holder.itemView.setBackgroundColor(meeting.getMeetingRoom().getMeetingRoomColor());
-
+        holder.roomColor.setColorFilter(meeting.getMeetingRoom().getMeetingRoomColor());
         holder.deleteMeetingButton.setOnClickListener(v -> {
             mApiService.deleteMeeting(meeting);
             notifyDataSetChanged();
